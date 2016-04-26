e42b905ef13a195f822c24452444517b4d76a14f
Martin Kamp Jensen <martin.kamp.jensen@schneider-electric.com>
2016-04-26 14:35:28 +0200

    Increate write load, including pbbench usage

--- scripts/master-write.sh
+++ scripts/master-write.sh
@@ -1,10 +1,53 @@
 #!/bin/bash
 
-counter=-1;
-while true
-do
-  echo "Writing to PostgreSQL (you have to manually abort)"
-  counter=$((counter+1))
-  psql -c "INSERT INTO data VALUES (${counter}, 'test')"
-  sleep 0.1
-done
+setup() {
+  SQL_SETUP_FILE="/tmp/setup.sql"
+  cat > ${SQL_SETUP_FILE} << EOL
+BEGIN;
+CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY, comment VARCHAR(100));
+TRUNCATE TABLE data;
+COMMIT;
+EOL
+  psql -f ${SQL_SETUP_FILE}
+}
+
+before() {
+  SQL_INSERT_FILE="/tmp/insert.sql"
+  echo "BEGIN;" > ${SQL_INSERT_FILE}
+  for ((i = 1; i <= ${MAX_ID}; i++))
+  do
+     echo "INSERT INTO data VALUES (${i}, null);" >> ${SQL_INSERT_FILE}
+  done
+  echo "COMMIT;" >> ${SQL_INSERT_FILE}
+  psql -f ${SQL_INSERT_FILE}
+}
+
+deleteAndReinsert() {
+  SQL_DELETE_FILE="/tmp/delete.sql"
+  SQL_REINSERT_FILE="/tmp/reinsert.sql"
+  while true
+  do
+    echo "BEGIN;" > ${SQL_DELETE_FILE}
+    echo "BEGIN;" > ${SQL_REINSERT_FILE}
+    IDS=`shuf -i1-${MAX_ID} -n ${OPERATIONS_PER_TRANSACTION}`
+    for i in ${IDS}
+    do
+      echo "DELETE FROM data WHERE id = ${i};" >> ${SQL_DELETE_FILE}
+      echo "INSERT INTO data VALUES(${i}, 'reinserted');" >> ${SQL_REINSERT_FILE}
+    done
+    echo "COMMIT;" >> ${SQL_DELETE_FILE}
+    echo "COMMIT;" >> ${SQL_REINSERT_FILE}
+    psql -f ${SQL_DELETE_FILE}
+    psql -f ${SQL_REINSERT_FILE}
+  done
+}
+
+run() {
+  MAX_ID=50000
+  OPERATIONS_PER_TRANSACTION=50
+  setup
+  before
+  deleteAndReinsert
+}
+
+run
--- scripts/slave-check.sh
+++ scripts/slave-check.sh
@@ -2,7 +2,6 @@
 
 while true
 do
-  echo "Checking PostgreSQL (you have to manually abort)"
   psql -c 'SELECT id, COUNT(*) FROM data GROUP BY id HAVING COUNT(*) > 1' | grep '0 rows' || exit
   sleep 1
 done
